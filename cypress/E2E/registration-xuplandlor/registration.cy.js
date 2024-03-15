c9494df214025a257c0ba45308c3b648acb1d9ff
Diegolacerda1 <128429943+Diegolacerda1@users.noreply.github.com>
2024-03-15 17:00:12 -0400

    manager and tenant registrations added

--- cypress/E2E/registration-xuplandlor/registration.cy.js
+++ cypress/E2E/registration-xuplandlor/registration.cy.js
@@ -16,4 +16,28 @@ describe('Registration functionality', () => {
         registrationPageObjects.inputAgreementCheckBox()
         // registrationPageObjects.inputSubmitBtn()
     });
+
+    it('should register as an Property Manager', () => {
+        registrationPageObjects.inputSignUp()
+        registrationPageObjects.inputFirstName()
+        registrationPageObjects.inputLastName()
+        registrationPageObjects.inputEmail()
+        registrationPageObjects.inputPassword()
+        registrationPageObjects.inputConfirmPassword()
+        registrationPageObjects.inputManagerBtn()
+        registrationPageObjects.inputAgreementCheckBox()
+        // registrationPageObjects.inputSubmitBtn()
+    });
+
+    it('should register as an Tenant', () => {
+        registrationPageObjects.inputSignUp()
+        registrationPageObjects.inputFirstName()
+        registrationPageObjects.inputLastName()
+        registrationPageObjects.inputEmail()
+        registrationPageObjects.inputPassword()
+        registrationPageObjects.inputConfirmPassword()
+        registrationPageObjects.inputTenantBtn()
+        registrationPageObjects.inputAgreementCheckBox()
+        // registrationPageObjects.inputSubmitBtn()
+    });
 });
