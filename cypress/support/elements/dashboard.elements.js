0e571b180c791d583bd311f8a0102bf0ef37b537
Diegolacerda1 <128429943+Diegolacerda1@users.noreply.github.com>
2024-04-05 15:19:02 -0400

    daily

--- cypress/support/elements/dashboard.elements.js
+++ cypress/support/elements/dashboard.elements.js
@@ -2,6 +2,6 @@ export const addNewProperty = {
     btnAddNewProperty : '.card-toolbar > .cursor-pointer',
     btnLetsAddIt : '.card-px > .btn',
     btnStudio : '.row > :nth-child(1) > .btn',
-    btnContinue : '#kt_create_account_form > .flex-stack > :nth-child(2) > .btn'
+    btnContinue : '#kt_create_account_form > .flex-stack > :nth-child(2) > .btn',
 
 }
