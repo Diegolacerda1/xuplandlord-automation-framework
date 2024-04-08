c132e4ae4a9898469c9d38edf99802c5a9c92c60
Diegolacerda1 <128429943+Diegolacerda1@users.noreply.github.com>
2024-04-08 12:04:28 -0400

    setting up API request for login functionality

--- cypress/E2E/addNewProperty-xuplandlord/addNewProperty.cy.js
+++ cypress/E2E/addNewProperty-xuplandlord/addNewProperty.cy.js
@@ -4,16 +4,16 @@ import loginPageObjects from "../../support/pageObjects/login.pageObjects";
 describe('Add New Property', () => {
     beforeEach(() => {
         cy.visit("/");
-        loginPageObjects.inputUserName()
-        loginPageObjects.inputPassword()
-        loginPageObjects.loginApp()
+        // loginPageObjects.inputUserName()
+        // loginPageObjects.inputPassword()
+        // loginPageObjects.loginApp()
       });
     it('should add a new property as owner', () => {
         dashboardPageObjects.btnAddNewProperty()
         dashboardPageObjects.btnLetsAddIt()
         dashboardPageObjects.btnStudio()
         dashboardPageObjects.btnContinue()
-        
+
     });
     
 });
