67bf8f8a23c5bfe3508f9c6ab34b8159b00920e4
Diegolacerda1 <128429943+Diegolacerda1@users.noreply.github.com>
2024-04-10 16:40:01 -0400

    deleted api request

--- cypress/E2E/addNewProperty-xuplandlord/addNewProperty.cy.js
+++ cypress/E2E/addNewProperty-xuplandlord/addNewProperty.cy.js
@@ -1,29 +1,21 @@
-import dashboardPageObjects from '../../support/pageObjects/dashboard.pageObjects' 
+import dashboardPageObjects from "../../support/pageObjects/dashboard.pageObjects";
 import loginPageObjects from "../../support/pageObjects/login.pageObjects";
 
-describe('Add New Property', () => {
-    beforeEach(() => {
-        cy.visit("/");
-        
-        // loginPageObjects.inputUserName()
-        // loginPageObjects.inputPassword()
-        // loginPageObjects.loginApp()
-        cy.request('POST', 'https://xup-landlord.herokuapp.com/auth/signin', { 
-    username: 'didi@gmail.com', 
-    password: 'Didi1234567'
-}).then(
-(response) => {
-window.localStorage.setItem('accessToken', response.body.accessToken)
-cy.log(response)
-}
-) 
-      });
-    it('should add a new property as owner', () => {
-        dashboardPageObjects.btnAddNewProperty()
-        dashboardPageObjects.btnLetsAddIt()
-        dashboardPageObjects.btnStudio()
-        dashboardPageObjects.btnContinue()
+describe("Add New Property", () => {
+  beforeEach(() => {
+    cy.visit("/")
 
-    });
+    loginPageObjects.inputUserName()
+    loginPageObjects.inputPassword()
+    loginPageObjects.loginApp()
     
+  });
+
+
+  it("should add a new property as owner", () => {
+    dashboardPageObjects.btnAddNewProperty();
+    dashboardPageObjects.btnLetsAddIt();
+    dashboardPageObjects.btnStudio();
+    dashboardPageObjects.btnContinue();
+  });
 });
