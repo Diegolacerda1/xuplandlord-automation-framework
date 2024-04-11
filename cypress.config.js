51f743e9fba7e4c0ad47f9f747ef5d0002dea273
Diegolacerda1 <128429943+Diegolacerda1@users.noreply.github.com>
2024-04-11 12:01:34 -0400

    daily

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      
+    setupNodeEvents(on, config) {  
     },
     baseUrl: 'https://xup-landlord-fronted.herokuapp.com/auth/login',
     watchForFileChanges: false
--- cypress/E2E/addNewProperty-xuplandlord/addNewProperty.cy.js
+++ cypress/E2E/addNewProperty-xuplandlord/addNewProperty.cy.js
@@ -4,25 +4,9 @@ import loginPageObjects from "../../support/pageObjects/login.pageObjects";
 describe("Add New Property", () => {
   beforeEach(() => {
     cy.visit("/")
-
-    // loginPageObjects.inputUserName()
-    // loginPageObjects.inputPassword()
-    // loginPageObjects.loginApp()
-    
-       
-      cy.request('POST', 'https://xup-landlord.herokuapp.com/auth/signin', { 
-      username: 'didi@gmail.com', 
-      password: 'Didi1234567'
-  }).then(
-  (response) => {
-
-    expect(response.status).to.equal(201)
-    expect(response.body).to.have.property('accessToken')
-  window.localStorage.setItem('accessToken', response.body.accessToken)
-  }
-  ); 
-  
-
+    loginPageObjects.inputUserName()
+    loginPageObjects.inputPassword()
+    loginPageObjects.loginApp()
   });
 
 
