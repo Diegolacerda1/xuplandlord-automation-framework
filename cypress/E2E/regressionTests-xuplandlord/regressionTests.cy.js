6e6165740514d2e1b5636d97da690bd387404443
Diegolacerda1 <128429943+Diegolacerda1@users.noreply.github.com>
2024-05-15 15:15:47 -0400

    regression test for Login page

--- cypress/E2E/regressionTests-xuplandlord/regressionTests.cy.js
+++ cypress/E2E/regressionTests-xuplandlord/regressionTests.cy.js
@@ -1,5 +1,20 @@
 import loginPageObjects from "../../support/pageObjects/login.pageObjects";
 
 describe('Regression XUP Landlord', () => {
+    context('Validate Login Page', () => {
+        beforeEach(() => {
+            cy.visit("/")
+        });
+
+        it('should successfully log in as an Owner with valid credentials', () => {
+            loginPageObjects.inputUserName();
+    loginPageObjects.inputPassword();
+    loginPageObjects.loginApp();
+    cy.get("#kt_header")
+      .should("be.visible")
+      .and("contain", "Property Managers");
+        });
+    });
+
     
 });
