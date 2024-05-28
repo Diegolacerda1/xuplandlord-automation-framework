1cf15bd777286f142c03ecd1dc74b0cdaa2044e0
Diegolacerda1 <128429943+Diegolacerda1@users.noreply.github.com>
2024-05-28 16:58:06 -0400

    daily

--- cypress/support/elements/dashboard.elements.js
+++ cypress/support/elements/dashboard.elements.js
@@ -1,5 +1,5 @@
 export const ADDNEWPROPERTYFORM = {
-    btnAddNewProperty : '.card-toolbar > .cursor-pointer',
+    btnAddNewProperty : '.card-toolbar > .cursor-pointer', 
     btnLetsAddIt : '.card-px > .btn',
     btnStudio : '.row > :nth-child(1) > .btn',
     btnContinue : '#kt_create_account_form > .flex-stack > :nth-child(2) > .btn',
